062e0a5d138d7712eea1b0f3a9ae622d80f1c516
denis <semenov200108@gmail.com>
2018-10-04 09:34:51 +0300

    Added some logic to main page. Now guessing app works fine

--- src/App.js
+++ src/App.js
@@ -9,41 +9,73 @@ class App extends Component {
 
         this.state = {
             words: [],
-            clickCount: 0
+            randomNumber: -1,
+            wordGuessed: 0,
+            guessedWordsCount: 0
         };
 
-        this.buttonListener = this.buttonListener.bind(this)
+        this.buttonListener = this.buttonListener.bind(this);
+        this.nextButtonTouch = this.nextButtonTouch.bind(this);
     }
 
     componentWillMount() {
-        axios.get("http://localhost:8090/main/words")
+        this.getWordsFromServer();
+    }
+
+    nextButtonTouch() {
+        this.setState({
+            wordGuessed: 0,
+            guessedWordsCount: this.state.guessedWordsCount + 1
+        });
+        this.getWordsFromServer()
+    }
+
+    getWordsFromServer() {
+        axios.get("http://localhost:8090/main/words/4")
             .then(res => {
                 const words = res.data;
-                console.log(words);
-                this.setState({words})
+                const randomNumber = Math.floor(Math.random() * words.length);
+                this.setState({
+                    words: words,
+                    randomNumber: randomNumber
+                })
             })
             .catch(ex => {
                 console.log(ex)
             })
     }
 
-    buttonListener(word) {
-        const clickCount = this.state.clickCount + 1;
-        console.log(word);
-        this.setState({clickCount: clickCount})
+    buttonListener(wordId) {
+        const rightWord = this.state.words.map(word => word)[this.state.randomNumber];
+        let wordGuessed = 0;
+        if (rightWord.id === wordId) {
+            wordGuessed = 1;
+        } else {
+            wordGuessed = -1;
+        }
+        this.setState({wordGuessed});
     }
 
     render() {
+        const translations = this.state.words.map(word => word.translate);
         const buttons = this.state.words.map(word =>
-            <button key={word.id} onClick={() => this.buttonListener(word.word)}>{word.word}</button>
+            <button key={word.id} onClick={() => this.buttonListener(word.id)}>{word.word}</button>
+        );
+        const nextButton = (
+            <button onClick={this.nextButtonTouch}>Next</button>
         );
         return (
             <div className="App">
-                <h1>Hello world</h1>
-                {/*<button onClick={() => this.buttonListener()}>{this.state.clickCount}</button>*/}
-                {buttons}
+                <h1>Guessed: {this.state.guessedWordsCount}</h1>
+                <h1>{translations[this.state.randomNumber]}</h1>
+                <div>
+                    {buttons}
+                </div>
+                <h1>{this.state.wordGuessed > 0 ? "Yeeey" : this.state.wordGuessed < 0 ? "Noooo" : ""}</h1>
+                {this.state.wordGuessed === 1 ? nextButton : null}
             </div>
         );
+
     }
 }
 
